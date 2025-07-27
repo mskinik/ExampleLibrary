abe41408ef8bcbcce68cba4ac90c76a99324d390
Mustafa Süleyman Kınık <suleymankinik@outlook.com>
2025-07-27 19:26:31 +0300

    Create capitilizeWords function

--- stringutils/src/main/java/com/mskinik/stringutils/StringUtils.kt
+++ stringutils/src/main/java/com/mskinik/stringutils/StringUtils.kt
@@ -2,4 +2,9 @@ package com.mskinik.stringutils
 
 object StringUtils {
     fun reverse(text: String): String = text.reversed()
+
+    fun capitalizeWords(text: String): String =
+        text.split(" ").joinToString(" ") { word ->
+            word.lowercase().replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
+        }
 }
